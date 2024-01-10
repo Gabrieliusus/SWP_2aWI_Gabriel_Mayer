a23f8cda678349ee94aea69872dcc8810055a229
Gabriel <gabrielsiegfried.mayer@gmail.com>
2024-01-10 21:42:09 +0100

    01_JS_Variablen_&_Operationen_DONE

---
+++ JS_Basics/JS_Basics.js
@@ -0,0 +1,36 @@
+console.log("Hallo");
+//Variablen
+let a = 7
+let b = 8
+let c = 10
+
+// Berechne folgendes und gib das Ergebnis über console.log aus
+
+let result = a + b + c
+console.log("a + b + c = " + result )
+// a + b + c
+result = (a + b) / c
+console.log("(a + b) / c = " + result )
+// (a + b) / c
+result = c - a + b
+console.log("c - a + b = " + result )
+// c - a + b
+result = a + b + c * 3
+console.log("a + b + c * 3 = " + result )
+// a + b + c * 3
+result = (a + b + c) / 4
+console.log("(a + b + c) / 4 = " + result )
+// (a + b + c) / 4
+
+// Ändere die Variable auf folgende Werte
+// a = 3
+a = 3
+// b = a + 3
+b = a + 3
+// c = c - 3
+c = c - 3
+
+// gib die neuen Werte der Variablen aus
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
