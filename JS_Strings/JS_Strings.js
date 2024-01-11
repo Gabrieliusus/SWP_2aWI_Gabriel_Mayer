284e90ddcc9720e7e69af8e422fe665cf49a496f
Gabriel <gabrielsiegfried.mayer@gmail.com>
2024-01-11 10:44:08 +0100

    02_JS_Strings_&_Zahlen_DONE

---
+++ JS_Strings/JS_Strings.js
@@ -0,0 +1,31 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+// +, -, *, /
+
+let c = 7.4
+let d = 7.5
+
+result = c + d
+console.log(result);
+
+// Texte Strings
+
+let name = "Gabriel"
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
+
+let e = 17.4
+let f = 3.9
+
+result = e / f
+
+console.log("The result of " + e + " divided by " + f + " is " + result);
+
+let firstname = "Gabriel"
+let lastname = "Mayer"
+
+console.log("Ich heiße " + firstname + " " + lastname)
