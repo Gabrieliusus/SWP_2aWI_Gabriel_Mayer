08686a56f71913cd417ecd30b0f5b90788645988
Gabriel Mayer <gabriel.mayer@student.htldornbirn.at>
2024-01-25 11:11:46 +0100

    JS_08_Switch_Statement_DONE

---
+++ JS_08_Switch_Statement/08-2.js
@@ -0,0 +1,27 @@
+let randomNumber = Math.random() * 6 + 5;
+let random = Math.floor(randomNumber);
+
+switch (random) {
+    case 10:
+        console.log("Ten");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    default:
+        // Handle default case if needed
+        break;
+}
+
---
+++ JS_08_Switch_Statement/08.js
@@ -0,0 +1,22 @@
+
+let randomNumber = Math.random() * 6 + 5;
+let random = Math.floor(randomNumber);
+
+if (random == 10) {
+    console.log("Ten")
+}
+else if (random == 9) {
+    console.log("Nine")
+}
+else if (random == 8) {
+    console.log("Eight")
+}
+else if (random == 7) {
+    console.log("Seven")
+}
+else if (random == 6) {
+    console.log("Six")
+}
+else if (random == 5) {
+    console.log("Five")
+}
